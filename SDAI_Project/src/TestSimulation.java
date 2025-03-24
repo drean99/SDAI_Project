8f8e1bcb7a3680170c7b48cd938476de87a65ef6
drean99 <62544019+drean99@users.noreply.github.com>
2025-03-24 17:01:38 +0100

    complex simulation

--- SDAI_Project/src/TestSimulation.java
+++ SDAI_Project/src/TestSimulation.java
@@ -40,17 +40,47 @@ public class TestSimulation {
             // 3. Crea un agente SimStepAgent per gestire i passi di simulazione
             mainContainer.createNewAgent("SimStepAgent", "SimStepAgent", null).start();
 
-            // 4. Scenario 1:
-            AgentController vehicle1 = mainContainer.createNewAgent("v1", "VehicleAgent", null);
+            AgentController vehicle = mainContainer.createNewAgent("v1", "VehicleAgent", null);
+            vehicle.start();
+            AgentController vehicle1 = mainContainer.createNewAgent("v2", "VehicleAgent", null);
             vehicle1.start();
-            AgentController vehicle2 = mainContainer.createNewAgent("v2", "VehicleAgent", null);
+            AgentController vehicle2 = mainContainer.createNewAgent("v3", "VehicleAgent", null);
             vehicle2.start();
-            AgentController vehicle3 = mainContainer.createNewAgent("v3", "VehicleAgent", null);
+            AgentController vehicle3 = mainContainer.createNewAgent("v4", "VehicleAgent", null);    
             vehicle3.start();
-            AgentController vehicle4 = mainContainer.createNewAgent("v4", "VehicleAgent", null);
+            AgentController vehicle4 = mainContainer.createNewAgent("v5", "VehicleAgent", null);
             vehicle4.start();
-            // AgentController vehicleAggressive1 = mainContainer.createNewAgent("vA1", "VehicleAggressiveAgent", null);
-            // vehicleAggressive1.start();
+            AgentController vehicle5 = mainContainer.createNewAgent("v6", "VehicleAgent", null);
+            vehicle5.start();
+            AgentController vehicle6 = mainContainer.createNewAgent("v7", "VehicleAgent", null);
+            vehicle6.start();
+            AgentController vehicle7 = mainContainer.createNewAgent("v8", "VehicleAgent", null);
+            vehicle7.start();
+            AgentController vehicle8 = mainContainer.createNewAgent("v9", "VehicleAgent", null);
+            vehicle8.start();
+            AgentController vehicle9 = mainContainer.createNewAgent("v10", "VehicleAgent", null);
+            vehicle9.start();
+            AgentController vehicle10 = mainContainer.createNewAgent("v11", "VehicleAgent", null);
+            vehicle10.start();
+            AgentController vehicle11 = mainContainer.createNewAgent("v12", "VehicleAgent", null);
+            vehicle11.start();
+            AgentController vehicle12 = mainContainer.createNewAgent("v13", "VehicleAgent", null);
+            vehicle12.start();
+            AgentController vehicle13 = mainContainer.createNewAgent("v14", "VehicleAgent", null);
+            vehicle13.start();
+            AgentController vehicle14 = mainContainer.createNewAgent("v15", "VehicleAgent", null);
+            vehicle14.start();
+            AgentController vehicle15 = mainContainer.createNewAgent("v16", "VehicleAgent", null);
+            vehicle15.start();
+            AgentController vehicle16 = mainContainer.createNewAgent("v17", "VehicleAgent", null);
+            vehicle16.start();
+            AgentController vehicle17 = mainContainer.createNewAgent("v18", "VehicleAgent", null);
+            vehicle17.start();
+            AgentController vehicle18 = mainContainer.createNewAgent("v19", "VehicleAgent", null);
+            vehicle18.start();
+
+            AgentController vehicleAggressive1 = mainContainer.createNewAgent("vA1", "VehicleAggressiveAgent", null);
+            vehicleAggressive1.start();
 
             // 5. Chiudi la connessione a SUMO
             //SumoConnector.close();
